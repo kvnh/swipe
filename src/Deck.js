1668216637d28bbb4319a20729738eecc0f305e0
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-29 13:58:19 +0100

    Callback for when swipe completes

--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
   constructor(props) {
     super(props);
 
@@ -31,7 +36,7 @@ class Deck extends Component {
       }
     });
 
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
   }
 
   forceSwipe(direction) {
@@ -42,10 +47,13 @@ class Deck extends Component {
     }).start(() => this.onSwipeComplete(direction));
   }
 
-  onSwipeComplete(direction) => {
-    const { onSwipeLeft, onSwipeRight } = this.props;
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
 
-    direction === 'right' ? onSwipeRight() : onSwipeLeft();
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
   }
 
   resetPosition() {
