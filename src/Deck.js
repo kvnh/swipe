03c4045e5bcb6c9f11b2b6c128e141733107de8b
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-29 14:42:43 +0100

    Give impression that there is a stack of cards

--- src/Deck.js
+++ src/Deck.js
@@ -98,7 +98,7 @@ class Deck extends Component {
       return (
         <Animated.View
           key={item.id}
-          style={styles.cardStyle}
+          style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}
         >
           {this.props.renderCard(item)}
         </Animated.View>
